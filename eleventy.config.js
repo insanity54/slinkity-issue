a00263fbc19912cbbb42ae538e7f3658daf220aa
Chris Grimmett <chris@grimtech.net>
2023-02-17 22:51:07 -0800

    init

---
+++ eleventy.config.js
@@ -0,0 +1,9 @@
+// .eleventy.js or eleventy.config.js
+const slinkity = require('slinkity')
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(slinkity.plugin, slinkity.defineConfig({
+
+  }))
+}
+
